c55e452d3302b93b30c8e7c56656ee8500353703
alaunal kauniyyah <alaunalkauniyyah3@gmail.com>
2020-04-08 19:38:29 +0700

    update of js

---
+++ src/assets/js/clocker.js
@@ -0,0 +1,36 @@
+
+window.addEventListener('load', () => {
+
+
+  function showTime() {
+
+      let date = new Date();
+      let h = date.getHours(); // 0 - 23
+      let m = date.getMinutes(); // 0 - 59
+      let s = date.getSeconds(); // 0 - 59
+      let session = "AM";
+
+      if (h == 0) {
+          h = 12;
+      }
+
+      if (h > 12) {
+          h = h - 12;
+          session = "PM";
+      }
+
+      h = (h < 10) ? "0" + h : h;
+      m = (m < 10) ? "0" + m : m;
+      s = (s < 10) ? "0" + s : s;
+
+      let time = h + ":" + m + ":" + s + " " + session;
+      document.getElementById("js-clock").innerText = time;
+      document.getElementById("js-clock").textContent = time;
+
+      setTimeout(showTime, 1000);
+
+  }
+
+  showTime();
+
+});
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,9 @@
 
 window.addEventListener('load', () => {
 
+  console.log('here main js loaded!');
+
+
     // const api = 'http://www.colr.org/json/color/random';
     // const body = document.querySelector('body');
 
@@ -22,37 +25,5 @@ window.addEventListener('load', () => {
     // randomColor();
 
     // setInterval(randomColor, 8000);
-
-
-    function showTime() {
-
-        let date = new Date();
-        let h = date.getHours(); // 0 - 23
-        let m = date.getMinutes(); // 0 - 59
-        let s = date.getSeconds(); // 0 - 59
-        let session = "AM";
-
-        if (h == 0) {
-            h = 12;
-        }
-
-        if (h > 12) {
-            h = h - 12;
-            session = "PM";
-        }
-
-        h = (h < 10) ? "0" + h : h;
-        m = (m < 10) ? "0" + m : m;
-        s = (s < 10) ? "0" + s : s;
-
-        let time = h + ":" + m + ":" + s + " " + session;
-        document.getElementById("js-clock").innerText = time;
-        document.getElementById("js-clock").textContent = time;
-
-        setTimeout(showTime, 1000);
-
-    }
-
-    showTime();
 
 });
